c11ea68d1718f7d7b6077d1a4945d260a3ef1440
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-14 23:51:44 -0500

    learned how to edit my button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <p className="text-red-500 font-bold">Hello word</p>
+    <div>
+      <Button />
+    </div>
   )
 }
