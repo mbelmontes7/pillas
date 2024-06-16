e4b69a115f319a6d9a2c120727f3ce1f6e104be4
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-15 23:17:04 -0500

    when you create a layout it applies everyhting to the parent compoents

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+//You can put anything inside RootLayout, and it will automatically inherit the styles defined in RootLayout.
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return <div className="h-full bg-red-500 text-white  ">{children}</div>;
 };
