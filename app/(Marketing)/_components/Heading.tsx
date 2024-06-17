73fab6c5600c08d9c4ba185840acf338ba38f561
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-17 09:10:38 -0500

    Added a brake to the text also the formating of the text is coming along.
    - need to center the text on the bigger screen

--- app/(Marketing)/_components/Heading.tsx
+++ app/(Marketing)/_components/Heading.tsx
@@ -5,8 +5,12 @@ export const Heading = () => {
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Tus ideas documentos y planes Bienvenido a{" "}
-        <span className="underline">Ponte pillas AI</span>
+        <span className="underline">Ponte pillas AI🦾</span>
       </h1>
+      <h3>
+        Ponte Pillas, el espacio conectado donde <br />
+        el trabajo se hace mejor y más rápido.
+      </h3>
     </div>
   );
 };
