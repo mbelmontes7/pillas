cbacbd7eee4b52dc1eb5a3f2cf7fcb455ed15606
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-17 09:19:44 -0500

    Added a buttton on the bottom of the text for the landing page. Also, I think the logo is going to stay as Pp not sure yet

--- app/(Marketing)/_components/Heading.tsx
+++ app/(Marketing)/_components/Heading.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
@@ -7,10 +8,14 @@ export const Heading = () => {
         Tus ideas documentos y planes Bienvenido a{" "}
         <span className="underline">Ponte pillas AI🦾</span>
       </h1>
-      <h3>
-        Ponte Pillas, el espacio conectado donde <br />
-        el trabajo se hace mejor y más rápido.
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Ponte Pillas, es el espacio que esta conectado con la inteligencia
+        artificial donde el trabajo se hace mejor y más rápido.
       </h3>
+      <Button>
+        Entra a Pp
+        <ArrowRight className="h-4 w-4 ml-2 " />
+      </Button>
     </div>
   );
 };
