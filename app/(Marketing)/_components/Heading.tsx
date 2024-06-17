42cbecee68add1903353a80552e7e7ad1fe9794a
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-16 23:15:01 -0500

    Adding a heading page to my app

---
+++ app/(Marketing)/_components/Heading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Tus ideas documentos y planes Bienvenido a{" "}
+        <span className="underline">Ponte pillas AI</span>
+      </h1>
+    </div>
+  );
+};
--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -1,6 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Heading } from "./_components/Heading";
 
 const MarketingPage = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items justify-center md:justidy-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  );
 };
 export default MarketingPage;
