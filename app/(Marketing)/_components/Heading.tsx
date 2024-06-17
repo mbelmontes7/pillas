03812a6d3fad1498ba62801c58272abf9654bfc1
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-17 18:12:24 -0500

    changes the discription of my text I am not sure if i want to keep it like this but so far I believe this is good this is located on the heading. tsx file

--- app/(Marketing)/_components/Heading.tsx
+++ app/(Marketing)/_components/Heading.tsx
@@ -6,11 +6,11 @@ export const Heading = () => {
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Tus ideas documentos y planes Bienvenido a{" "}
-        <span className="underline">Ponte pillas AI🦾</span>
+        <span className="underline">Pp AI</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Ponte Pillas, es el espacio que esta conectado con la inteligencia
-        artificial donde el trabajo se hace mejor y más rápido.
+        Ponte Pilas, es el espacio que esta conectado con la inteligencia
+        artificial donde puedes tomar apuntes de manera mejor y sensilla.
       </h3>
       <Button>
         Entra a Pp
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ponte Pillas",
+  title: "Ponte las Pilas",
   description: "Una app donde puedes usar AI a que te ayude con tus apuntes.",
   //added the favicons here
   icons: {
