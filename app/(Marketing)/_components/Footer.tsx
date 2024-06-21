d616f363a743bd4b585dceed3a64ad4c7707a8c0
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-20 23:58:38 -0500

    Adding the flex items to the background

--- app/(Marketing)/_components/Footer.tsx
+++ app/(Marketing)/_components/Footer.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 
 const Footer = () => {
   return (
+    //this add the w- tho the background
     <div className="flex items-center w-full p-1 bg-background dark:bg-[#1f1f1f] z-50 ">
       <Logo />
       {/* //.md:ml-auto: On medium screens and above, apply margin-left: auto;
