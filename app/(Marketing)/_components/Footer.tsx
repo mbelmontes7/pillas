736177959dfd57dbb6380d223b25daac0be8c1ab
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-20 23:55:35 -0500

    Adding a thing to the foooter

--- app/(Marketing)/_components/Footer.tsx
+++ app/(Marketing)/_components/Footer.tsx
@@ -6,9 +6,10 @@ const Footer = () => {
     <div className="flex items-center w-full p-1 bg-background dark:bg-[#1f1f1f] z-50 ">
       <Logo />
       <div
-        className="md:ml-auto w-full justify-between md:justify-end 
-      flex items-center gap-x-2 text-muted-foreground"
+        className="md:ml-auto w-full justify-between md:justify-end lg:justify-items-end
+      flex items-center gap-x-2 text-muted-foreground "
       >
+        {/* //variant para que no se mira  */}
         <Button variant="ghost" size="sm">
           Privacy Policy
         </Button>
