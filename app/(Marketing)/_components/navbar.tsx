5fc1199a26e8cecee25ab42643b9d9d5936e684b
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-21 22:46:29 -0500

    return statement was  not properly enclosing the JSX. In JavaScript. Remember when place a line break immediately after a return statemen

--- app/(Marketing)/_components/navbar.tsx
+++ app/(Marketing)/_components/navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
-//this is going to be my navbar component where is going to be on top of the page
+
+// This is going to be my navbar component where it is going to be on top of the page
 const Navbar = () => {
   return <div>navbar!</div>;
 };
+
 export default Navbar;
