5c3e333ecdf82b065330f7b56fac9bd318e9490d
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-23 15:47:18 -0500

    So, I was gettting an error earlier because my navbar did not had a export default for my navbar Using a default export simplifies the import statement in other files. You can import the default export with any name you choose.

--- app/(Marketing)/_components/navbar.tsx
+++ app/(Marketing)/_components/navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
 
 // This is going to be my navbar component where it is going to be on top of the page
-const Navbar = () => {
+export const Navbar = () => {
   const scrolled = useScrollTop();
 
   //return something here
-  return <div>navbar</div>;
+  return <div className={cn()}>navbar</div>;
 };
-
 export default Navbar;
