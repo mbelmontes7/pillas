72fb394fceaba5538cf284a1b37caf197c7c1970
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-26 16:41:56 -0500

    Added a better way of saying thing on my heading I am not sure at this moment if i want to do add the type writing effect sounds intersting, but not sure yet and it would be located on the heading

--- app/(Marketing)/_components/Heading.tsx
+++ app/(Marketing)/_components/Heading.tsx
@@ -5,8 +5,8 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Tus ideas documentos y planes Bienvenido a{" "}
-        <span className="underline">Pp AI</span>
+        Tus ideas documentos y planes
+        {/* <span className="underline">Pp AI</span> */}
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Ponte Pilas, es el espacio que esta conectado con la inteligencia
--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -5,7 +5,7 @@ const MarketingPage = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       {/* Ensuring inner div also centers its children */}
-      <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10 max-w-7xl">
+      <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10 ">
         <Heading />
         <Heroes />
       </div>
