3ccc63b594a051c657628c27f6db09e2e2c55d91
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-27 18:15:38 -0500

    added a spinner here and logic for the auth to the user to get into the app then import on top also      wrap the button to the auth where the logic is is to render to acually enter to pp

--- app/(Marketing)/_components/Heading.tsx
+++ app/(Marketing)/_components/Heading.tsx
@@ -1,7 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +16,15 @@ export const Heading = () => {
         Ponte Pilas, es el espacio que esta conectado con la inteligencia
         artificial donde puedes tomar apuntes de manera mejor y sensilla.
       </h3>
-      <Button>
-        Entra a Pp
-        <ArrowRight className="h-4 w-4 ml-2 " />
-      </Button>
+      {/* //added a spinner here and logic for the auth to the user to get into the app then import on top also 
+     wrap the button to the auth where the logic is is to render to acually enter to pp  */}
+      {isLoading && <Spinner size="lg" />}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          Entra a Pp
+          <ArrowRight className="h-4 w-4 ml-2 " />
+        </Button>
+      )}
     </div>
   );
 };
