a52e3d9780239d907b6d53d34e75d0f8fb8a3086
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-28 15:05:45 -0500

    importing the AUTH into the main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 // keep in mind that  Import and render components within MainLayout for reusable layouts and organized code which now is making so much sense.
 "use client";
-
+import { useConvexAuth } from "convex/react";
 // Define the MainLayout component
 // The component accepts a prop called 'children'
 const MainLayout = ({
@@ -9,6 +9,8 @@ const MainLayout = ({
   // Specify that 'children' can be any valid React node
   children: React.ReactNode;
 }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   // Return a div that wraps around the children prop
   return <div>{children}</div>;
 };
