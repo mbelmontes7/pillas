7ba8df951981a9e18f6c68bbd4f24ef345162298
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-29 00:08:55 -0500

    Add Navigation component with structured sidebar template and hover effect
    - Created a Navigation component with a sidebar layout.
    - Included action items and documents sections within the sidebar.
    - Implemented hover effect to reveal additional sidebar content.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,8 +3,18 @@
 export const Navigation = () => {
   return (
     <>
+      {/* //templte for the side bar  */}
       <aside className="group/sidebar h-full bg-slate-100 overflow-y-auto relative flex w-60 flex-col z-[9999]">
-        Navi
+        {/* //action items */}
+        <div>
+          <p>Action items</p>
+        </div>
+        {/* //this is going to another sections */}
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        {/* //Implemented hover effect to reveal additional sidebar content. */}
+        <div className="opacity-0 group-hover:opacity-100" />
       </aside>
     </>
   );
