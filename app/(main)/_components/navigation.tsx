4d189b9415aa64b2416aaa69142dfae6e17161a6
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-06-29 18:12:59 -0500

    Add mouse event handlers for sidebar resizing
    - Implemented handleMouseDown to start resizing.
    - Added handleMouseMove to adjust sidebar width during resize.
    - Included handleMouseUp to clean up after resizing is complete.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronsLeft } from "lucide-react";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 //npm i usehooks-ts library from
 import { useMediaQuery } from "usehooks-ts";
@@ -18,7 +18,43 @@ export const Navigation = () => {
   const navbarRef = useRef<ElementRef<"div">>(null);
   //Logic to handle sidebar state when `isResetting` changes
   const [isResetting, setIsResetting] = useState(false);
+  //The useState hook is used in this context to manage the state of the component dynamically based on different conditions.
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    let newWidth = event.clientX;
+    //how much the user can rezise the sidebar and how wide it can go, so we add the values
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+    //
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      //calculate for the value
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`,
+      );
+    }
+  };
+  //all this on
+  const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
 
   return (
     <>
@@ -33,9 +69,13 @@ export const Navigation = () => {
         )}
       >
         <div
+          //button for the side bar for the mobile here
           role="button"
           // Import chevronleft from the lucid library and it made this cute icon for my side bar added css to make it look nicer only when the user hovers and when you can see it
-          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+          className={cn(
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100",
+          )}
         >
           <ChevronsLeft h-6 w-6 />
         </div>
@@ -51,8 +91,27 @@ export const Navigation = () => {
                     -Changes the cursor to indicate that the element is resizable horizontally (east-west direction)
                     -duration-300: Sets the transition to last 300 milliseconds.
                 */}
-        <div className="opacity-0 group-hover/sidebar:opacity-100 transition-opacity duration-300 ease-in-out cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+        <div
+          onMouseDown={handleMouseDown}
+          // onClick={resetWidth}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition-opacity duration-300 ease-in-out cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+        />
       </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full",
+        )}
+      >
+        <nav className="bg-transparent px-3 py-2 w-full">
+          {" "}
+          {isCollapsed && (
+            <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+          )}
+        </nav>
+      </div>
     </>
   );
 };
