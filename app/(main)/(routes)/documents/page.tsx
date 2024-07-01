86238f554585dd61d57c6794012904544a05271e
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-01 10:38:06 -0500

    adding "Create a note" button to documents page
    This commit adds a button to the documents page that allows users to create a new note. The button is styled with the Lucide React icon library and the button component from the UI library. This enhancement improves the user experience by providing a clear call-to-action for creating notes.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,14 @@
 import Image from "next/image";
 import React from "react";
 import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const DocumentsPage = () => {
   const { user } = useUser();
 
   return (
-    <div className="h-full flex flex-col items-center justify-center space-x-4">
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
         src="/empty.png"
         alt="Empty"
@@ -23,7 +25,13 @@ const DocumentsPage = () => {
         className="hidden dark:block"
       />
       {/* //the logic here is to show the user name on the documents page */}
-      <h2>Bienvienido a {user?.firstName}&apos;s documentos</h2>
+      <h2 className="text-lg font-semibold max-h-min dark:text-white">
+        Bienvienido a {user?.firstName}&apos;s documentos
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Crea una nota
+      </Button>
     </div>
   );
 };
